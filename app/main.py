6776e3ffc746ae645f126902a3d64d406d1313ec
Przemysław Białoń <przem0.b95@gmail.com>
2024-06-09 18:04:44 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,8 +7,9 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept()
-    print("Connection from: ", addr)
-    conn.sendall(PONG)
+    while True:
+        conn.recv(1024)
+        conn.sendall(PONG)
 
 
 if __name__ == "__main__":
