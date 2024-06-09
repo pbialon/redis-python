f7581b5fdd3c9b44d6fc3a7a086500d15ea2ac67
Przemysław Białoń <przem0.b95@gmail.com>
2024-06-09 21:28:38 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,29 @@
 import socket
+import threading
 
 PONG = b"+PONG\r\n"
+
+def handle_connection(conn, addr):
+    print(f"Connection from {addr} in thread {threading.current_thread().name}")
+    with conn:
+        while True:
+            conn.recv(1024)
+            conn.sendall(PONG)
 
 def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept()
+
+    threads = []
     while True:
-        conn.recv(1024)
-        conn.sendall(PONG)
+        conn, addr = server_socket.accept()
+        thread = threading.Thread(target=handle_connection, args=(conn, addr))
+        threads.append(thread)
+        thread.start()
+    
+    for thread in threads:
+        thread.join()
 
 
 if __name__ == "__main__":
